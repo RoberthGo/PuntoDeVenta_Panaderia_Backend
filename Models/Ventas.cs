d2db2f89e871d2484aa6b0cb2eff0bc431c22a91
leneza41 <leonar4145@gmail.com>
2025-11-27 20:39:14 -0600

    Added Ventas & Detalle Ventas

---
+++ Models/Ventas.cs
@@ -0,0 +1,10 @@
+﻿namespace Panaderia.Models
+{
+    public class Ventas
+    {
+        public int IdVenta { get; set; }
+        public DateTime Fecha { get; set; }
+        public int IdEmpleado { get; set; }
+        public decimal Total { get; set; }
+    }
+}
